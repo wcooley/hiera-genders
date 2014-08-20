a43ea2175d926f7b8937e161b41f1068af728805
Wil Cooley <wcooley@nakedape.cc>
2014-08-20 15:49:22 -0700

    Update tests with correct results for unfound keys

--- spec/functions/hiera_genders_good_spec.rb
+++ spec/functions/hiera_genders_good_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
+item_unfound_msg = 'Could not find data item \w+ in any Hiera ' +
+  'data file and no default supplied'
+
 describe 'hiera' do
   context 'hiera-genders backend' do
     context 'good genders file' do
@@ -16,15 +19,17 @@ describe 'hiera' do
           should run.with_params('oof').and_return(true)
         end
 
-        it 'looks up a non-existent attribute and gets false' do
-          should run.with_params('does_not_exist').and_return(false)
+        it 'looks up a non-existent attribute and an error is raised' do
+          should run.with_params('does_not_exist') \
+            .and_raise_error(Puppet::Error, /#{item_unfound_msg}/)
         end
       end
 
       context 'unknown hostname' do
         let(:facts) {{ :hostname => 'host_that_isnt' }}
-        it 'looks up a variable and gets false' do
-          should run.with_params('foo').and_return(false)
+        it 'looks up a variable and an error is raised' do
+          should run.with_params('foo') \
+            .and_raise_error(Puppet::Error, /#{item_unfound_msg}/)
         end
       end
     end
